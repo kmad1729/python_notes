1bc98239d58b9f08a762aa2f70fbfea6eaeeb8ce
Kashyap Maduri <kashyapmaduri@outlook.com>
2015-10-13 21:29:24 -0700

    adding some more notes on metaprogramming

--- metaprogramming/debugly.py
+++ metaprogramming/debugly.py
@@ -5,6 +5,7 @@ from functools import  wraps, partial
 def debug(func=None, *, prefix = ''):
     if func is None:
         return partial(debug, prefix = prefix)
+    print('func adfadfad', func.__name__)
     #func is function to be wrapped
     msg = prefix + func.__qualname__ + prefix
     @wraps(func)
