3c447db1696891fcfd918aa5ac0cfdabe1bb6290
Kashyap Maduri <kashyapmaduri@outlook.com>
2015-10-15 22:54:57 -0700

    starting code for different data structures

---
+++ metaprogramming/start.py
@@ -0,0 +1,19 @@
+#start.py
+
+class Sock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Address:
+    def __init__(self, hostname, port):
+        self.hostname = hostname
+        self.port = port
+   
+
