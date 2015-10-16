1799987fede78257fb733cf8870d4b2c5aaa6ce0
Kashyap Maduri <kashyapmaduri@outlook.com>
2015-10-15 23:01:52 -0700

    easier way of writing init

--- metaprogramming/start.py
+++ metaprogramming/start.py
@@ -1,19 +1,17 @@
 #start.py
 
-class Sock:
-    def __init__(self, name, shares, price):
-        self.name = name
-        self.shares = shares
-        self.price = price
+class Structure:
+    _fields = []
+    def __init__(self, *args):
+        for name, val in zip(self._fields, args):
+            setattr(self, name, val)
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class Sock(Structure):
+    _fields = ['name', 'shares', 'price']
 
-class Address:
-    def __init__(self, hostname, port):
-        self.hostname = hostname
-        self.port = port
+class Point(Structure):
+    _fields = ['x', 'y']
+
+class Address(Structure):
+    _fields = ['hostname', 'port']
    
-
