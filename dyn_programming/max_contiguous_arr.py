f2247ca2c8b6b069213d1aa4d9cc0023a661eb6d
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-01-31 23:44:21 -0800

    adding some more test cases

--- dyn_programming/max_contiguous_arr.py
+++ dyn_programming/max_contiguous_arr.py
@@ -12,7 +12,11 @@ def get_max_contiguous_sum(num_list):
 
     for i in range(1, len(num_list)):
         aux_arr[i] = max(aux_arr[i - 1] + num_list[i], 0)
-        max_at_i[i] = max(max_at_i[i - 1], aux_arr[i])
+        if(max_at_i[i - 1] < 0):
+            max_at_i[i] = max(max_at_i[i - 1], num_list[i])
+        else:
+            max_at_i[i] = max(max_at_i[i - 1], aux_arr[i])
+
 
     return max_at_i[-1]
 
@@ -22,6 +26,21 @@ class TestChangePossible(unittest.TestCase):
         self.assertEqual(5, get_max_contiguous_sum([5, -3, -4, -5]))
         self.assertEqual(55, 
                 get_max_contiguous_sum([5, 15, -30, 10, -5, 40, 10]))
+
+        self.assertEqual(55, 
+                get_max_contiguous_sum([5, 15, -30, 10, -5, 40, 10, -5]))
+
+        self.assertEqual(-4, 
+                get_max_contiguous_sum([-23, -5, -4]))
+
+        self.assertEqual(0, 
+                get_max_contiguous_sum([-23, -5, -4, 0]))
+
+        self.assertEqual(4, 
+                get_max_contiguous_sum([-23, -5, -4, 0, 4]))
+
+        self.assertEqual(4, 
+                get_max_contiguous_sum([-23, 4, -5, -4, 0, 4]))
 
 if __name__ == "__main__":
     unittest.main()
