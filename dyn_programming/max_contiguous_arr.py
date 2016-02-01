c32b3506d4ecf18c1f27873eed577cb2ed4648bb
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-01-31 23:35:46 -0800

    adding max contiguous sum problem

---
+++ dyn_programming/max_contiguous_arr.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+'given a bag of infinite different valued coins check if a change exists'
+
+import unittest
+
+def get_max_contiguous_sum(num_list):
+    aux_arr = [0] * len(num_list)
+    max_at_i = [0] * len(num_list)
+
+    aux_arr[0] = max(0, num_list[0])
+    max_at_i[0] = num_list[0]
+
+    for i in range(1, len(num_list)):
+        aux_arr[i] = max(aux_arr[i - 1] + num_list[i], 0)
+        max_at_i[i] = max(max_at_i[i - 1], aux_arr[i])
+
+    return max_at_i[-1]
+
+class TestChangePossible(unittest.TestCase):
+
+    def test_get_max_sum(self):
+        self.assertEqual(5, get_max_contiguous_sum([5, -3, -4, -5]))
+        self.assertEqual(55, 
+                get_max_contiguous_sum([5, 15, -30, 10, -5, 40, 10]))
+
+if __name__ == "__main__":
+    unittest.main()
+
