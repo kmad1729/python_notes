2426add5c2bff348bd8d47c80b111da2c5d0cf80
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-02-22 23:25:44 -0800

    writing a program to generate all choices

---
+++ gen_progs/gen_all_choices.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''Given n sets of choices: (1,2,3), (2,3,4), (4,5) You pick one element from 
+each set of choices. Generate all possible picking.
+'''
+
+def get_all_choices(*args):
+
+    pass
+
+l1 = [(1,2), (5,6)]
+#output should be (1, 5), (1, 6), (2, 5), (2, 6)
+l1 = [(1,2,3), (2,3,4), (4, 5)]
